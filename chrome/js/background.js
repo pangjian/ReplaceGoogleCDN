1c3647f9412bb0f1bc78403d9bfdf4f0a65e5a5c
pangjian <pangjian1010@gmail.com>
2018-05-30 11:19:04 +0800

    增加替换源

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,13 +1,20 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(request) {
-        var url = request.url.replace('googleapis.com', 'lug.ustc.edu.cn');
+        var url = request.url.replace('ajax.googleapis.com', 'ajax.loli.net');
+        url = url.replace('fonts.googleapis.com', 'fonts.loli.net')
         url = url.replace('themes.googleusercontent.com', 'google-themes.lug.ustc.edu.cn');
+        url = url.replace('cdnjs.cloudflare.com', 'cdnjs.loli.net')
+        url = url.replace('gravatar.com', 'gravatar.loli.net')
         return {redirectUrl: url};
     },
     {
         urls: [
             "*://ajax.googleapis.com/*",
-            "*://themes.googleusercontent.com/*"
+            "*://fonts.googleapis.com/*",
+            "*://themes.googleusercontent.com/*",
+            "*://cdnjs.cloudflare.com/*",
+            "*://*.gravatar.com/*"
+
         ]
     },
     ["blocking"]
